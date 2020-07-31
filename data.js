859dd0e947d39acd94531d854e5902de0248c206
unknown <julialdbrito@gmail.com>
2020-07-31 13:53:59 -0300

    Module02

---
+++ data.js
@@ -0,0 +1,41 @@
+module.exports = [
+    {
+        id: 'vqrjFnq3-uo',
+        title: 'Criando Player de Áudio com JavaScript | Mayk Brito',
+        duration: '1h e 45 min',
+        price: 'Free',
+        featured: true
+    },
+    {
+        id: 'GykTLqODQuU',
+        title: 'Formulário Animado com JS puro e CSS Animation | Mayk Brito',
+        duration: '57 min',
+        price: 'Free'
+    },
+    {
+        id: 'GSqR2i-Pq6o',
+        title: 'Desvendando a variável this no JavaScript | Mayk Brito',
+        duration: '48 min',
+        price: 'Free',
+        featured: true
+    },
+    {
+        id: 'HN1UjzRSdBk',
+        title: 'Desvendando o CSS grid na prática | Mayk Brito',
+        duration: '36 min',
+        price: 'Free'
+    },
+    {
+        id: 'ghTrp1x_1As',
+        title: 'O que é API? REST e RESTful | Mayk Brito',
+        duration: '33 min',
+        price: 'Free',
+        featured: true
+    },
+    {
+        id: 'rAzHvYnQ8DY',
+        title: 'Array: Higher Oreder Functions | Mayk Brito',
+        duration: '54 min',
+        price: 'Free'
+    }
+]
